523fa02f2671c80c4e0c0a000a18e1c24028ad3c
VedaRuthvik01 <svruthvik143@gmail.com>
2025-04-27 12:31:29 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import List from './components/List';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
