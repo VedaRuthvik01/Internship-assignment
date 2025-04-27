7e93bc4b9757be6cbc4b28ecf53f2ae123b0c6b4
VedaRuthvik01 <svruthvik143@gmail.com>
2025-04-27 12:34:29 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#282c34', color: 'white', padding: '10px', textAlign: 'center' }}>
+      <h1>Internship Task Manager</h1>
+    </header>
+  );
+};
+
+export default Header;
